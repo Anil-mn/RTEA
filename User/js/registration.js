020603e608912fb6579515428b0dc2433723f9f4
Sreelekhatk <54243520+Sreelekhatk@users.noreply.github.com>
2020-01-31 20:17:19 +0530

    user registration

---
+++ User/js/registration.js
@@ -0,0 +1,14 @@
+
+
+
+
+var email = document.getElementById('email');
+var userName = document.getElementById('userName');
+var phoneNo = document.getElementById('phoneNo');
+
+
+var address = document.getElementById('address');
+var nation = document.getElementById('nation');
+var doB= document.getElementById('doB');
+var passWord = document.getElementById('passWord');
+var confirm = document.getElementById('confirm');
