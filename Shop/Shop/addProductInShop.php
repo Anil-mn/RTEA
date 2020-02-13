b0ede1e33e11f0e485e33bd2bd98fd89abcd21a5
ANIL MN <amn.nmb@gmail.com>
2020-02-13 22:04:38 +0530

    Shop Product added ,Shop Id has to complete

--- Shop/Shop/addProductInShop.php
+++ Shop/Shop/addProductInShop.php
@@ -6,12 +6,15 @@ echo $superSub;
  $numberOfProducts =$_POST['noofproduct'];
  $location = $_POST['location'];
  $productItem = $_POST['productItem'];
- 
+ $shopId=1;
+ $check = mysqli_query($con, "SELECT * FROM `shop_products` where `Name` = '$productItem'");
+ while($row = mysqli_fetch_array($check))
+                     {
+                         $productID=$row[0];
+                     }
 
-
-
-//  $query= mysqli_query($con,"INSERT INTO `shop_link`(`Product_ID`, `Shop_ID`, `NumberOf`, `Location`) 
-//  VALUES  ('','$productItem','','')";
+$query= mysqli_query($con,"INSERT INTO `shop_link`(`Product_ID`, `Shop_ID`, `NumberOf`, `Location`) 
+VALUES  ('$productID',' $shopId','$numberOfProducts','$location')";
 
 
 ?>
