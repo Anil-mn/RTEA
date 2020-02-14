8d6513088474d4c8865c4fb33075756815694725
yadhu-cu <59932587+yadhu-cu@users.noreply.github.com>
2020-02-14 14:16:44 +0530

    Login completed

--- BackEnd/Php/User/loginInvent.php
+++ BackEnd/Php/User/loginInvent.php
@@ -1,9 +1,9 @@
 <?php
 include('../connection.php');
- $companyName = $_POST ['phoneNumber'];
+ $companyno = $_POST ['phoneNumber'];
  $password = $_POST['password'];
 
- $query = "select * from inventory where companyName = '$companyName' and password = '$password'";
+ $query = "select * from inventory where id = '$companyno' and password = '$password'";
  $result = mysqli_query($con, $query);
  $check = mysqli_fetch_array($result);
  if ($check == true){
