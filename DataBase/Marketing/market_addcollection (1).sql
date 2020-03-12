84f4be4c9f6913d4cafb618f43f6794de6ebbcd1
Sreelekhatk <54243520+Sreelekhatk@users.noreply.github.com>
2020-03-12 12:22:43 +0530

    new db updated

--- DataBase/Marketing/market_addcollection (1).sql
+++ DataBase/Marketing/market_addcollection (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2020 at 07:47 AM
+-- Generation Time: Mar 12, 2020 at 07:48 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -23,36 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `distributor_req`
+-- Table structure for table `market_addcollection`
 --
 
-CREATE TABLE `distributor_req` (
-  `ProductReq_ID` int(11) NOT NULL,
-  `shop_ID` int(11) NOT NULL,
-  `Product_ID` int(11) NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `date` date NOT NULL
+CREATE TABLE `market_addcollection` (
+  `AD_ID` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `ad_place` varchar(30) NOT NULL,
+  `discount` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `market_addcollection`
+--
+
+INSERT INTO `market_addcollection` (`AD_ID`, `id`, `date`, `ad_place`, `discount`) VALUES
+(1, 8, '2020-04-12', '2000', '50%');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `distributor_req`
+-- Indexes for table `market_addcollection`
 --
-ALTER TABLE `distributor_req`
-  ADD PRIMARY KEY (`ProductReq_ID`);
+ALTER TABLE `market_addcollection`
+  ADD PRIMARY KEY (`AD_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `distributor_req`
+-- AUTO_INCREMENT for table `market_addcollection`
 --
-ALTER TABLE `distributor_req`
-  MODIFY `ProductReq_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `market_addcollection`
+  MODIFY `AD_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- DataBase/Marketing/market_membership (1).sql
+++ DataBase/Marketing/market_membership (1).sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2020 at 03:23 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Mar 12, 2020 at 07:49 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,15 +23,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `market_addcollection`
+-- Table structure for table `market_membership`
 --
 
-CREATE TABLE `market_addcollection` (
-  `AD_ID` int(11) NOT NULL,
-  `id` int(10) NOT NULL,
-  `date` date NOT NULL,
-  `ad_place` varchar(30) NOT NULL,
-  `discount` varchar(20) NOT NULL
+CREATE TABLE `market_membership` (
+  `mem_ID` int(10) NOT NULL,
+  `add_id` int(10) NOT NULL,
+  `mem_type` varchar(20) NOT NULL,
+  `ad_Shop` varchar(20) NOT NULL,
+  `ad_Slot` varchar(20) NOT NULL,
+  `Price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -41,22 +40,20 @@ CREATE TABLE `market_addcollection` (
 --
 
 --
--- Indexes for table `market_addcollection`
+-- Indexes for table `market_membership`
 --
-ALTER TABLE `market_addcollection`
-  ADD PRIMARY KEY (`AD_ID`);
+ALTER TABLE `market_membership`
+  ADD PRIMARY KEY (`mem_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `market_addcollection`
+-- AUTO_INCREMENT for table `market_membership`
 --
-ALTER TABLE `market_addcollection`
-  MODIFY `AD_ID` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+ALTER TABLE `market_membership`
+  MODIFY `mem_ID` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
