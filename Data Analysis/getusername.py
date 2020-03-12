11be80c91be984219f78419510258ed14aa6ae47
ANIL MN <amn.nmb@gmail.com>
2020-03-12 10:57:50 +0530

    aprooir alg added

--- Data Analysis/getusername.py
+++ Data Analysis/getusername.py
@@ -1,4 +1,3 @@
-# from connection import mydb
 # https://stackabuse.com/association-rule-mining-via-apriori-algorithm-in-python/
 
 # mycursor = mydb.cursor()
@@ -14,3 +13,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 
+store_data = pd.read_csv('A://Datasets/store_data.csv') 
+store_data.head()
+num_records = len(store_data.head())
+print(num_records)
+records = []
+for i in range(0, num_records):
+     records.append([str(store_data.values[i,j]) for j in range(0, 20)])
+
+association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
+association_results = list(association_rules)
+print(association_rules )
