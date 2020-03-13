07c70d91841a1b2c01bfd65a1eeaf9e1a481d5eb
AmithaSuresh <amithasuresh097@gmail.com>
2020-03-13 21:33:43 +0530

    error

--- User/user/Process/ProductDeletion.php
+++ User/user/Process/ProductDeletion.php
@@ -1,15 +1,24 @@
 <?php
  include('../../../BackEnd/php/connection.php');
-$prname=
+ 
+
+
+ 
+$prodname=$_POST['prodname'];
+$quantity=$_POST['quantity'];
+
+$check=mysqli_query($con,"SELECT * from `shop_products` where `Name`='$prodname'");
+                                   while($row1=mysqli_fetch_array($check))
+                                   {
+                                     $prodid=$row1[0];
+                                     $price=$row1[2];
+                                    }
+   
  if(isset($_POST['submit1']))
  {
-     $query="INSERT into"
- }
-
-
-
-
-
-
+     $query1="INSERT into `user_cart` (`ProductID`, `Numberofprod`,`price`,`onlineID`) values ('$prodid','$quantity','$price','$onlineid')";
+     $result=mysqli_query($con,$query1);   
+      
+ }     
 
 ?>
