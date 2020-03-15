d18f5ea04bb219792790cb6268ed7d4317eb08dd
ANIL MN <amn.nmb@gmail.com>
2020-03-15 10:38:04 +0530

    updated

--- Marketing/Marketing/generateAds.php
+++ Marketing/Marketing/generateAds.php
@@ -9,10 +9,14 @@ if(!isset($_SESSION['id'])){
  
 }
 
+
+
+
+
 Function GenAdd(){
     
 $id=$_SESSION['id'];
-
+// Collectiong Add id and product name
 include('../../BackEnd/Php/connection.php');
 $marketADs = mysqli_query($con,"SELECT * FROM `market_add` Order by `ADD_ID` DESC limit 1");
 while($row=mysqli_fetch_array($marketADs)){
@@ -22,21 +26,86 @@ while($row=mysqli_fetch_array($marketADs)){
 }
 
 
+//selecting product ID
 $SelectProductId = mysqli_query($con,"SELECT * FROM `shop_products` WHERE `Name` like '%$productName%'");
 while($row=mysqli_fetch_array($SelectProductId)){
     $productID = $row[0];
+    
+    
     //echo $productID;
 }
 
 
-$shops = mysqli_query($con,"SELECT count(`Product_ID`),max(`NumberOf`),count(`Shop_ID`) FROM `shop_link` WHERE `Product_ID` = '$productID' and `NumberOf` > 30");
-while($row=mysqli_fetch_array($shops)){
-    $numberofshops = $row[2];
-    $numberofproducts = $row[1];
-    echo $numberofshops;
-    echo $numberofproducts ;
+
+$addDetails = mysqli_query($con,"SELECT * FROM `market_addcollection` Where `id` = '$add_id'");
+while($row=mysqli_fetch_array($addDetails)){
+    $DueDate= $row[2];
+    //echo $DueDate;
 }
 
+
+
+
+$ShopDetais =  mysqli_query($con,"SELECT * FROM `shop_link` WHERE `Product_ID` = '$productID' and `NumberOf` > 30");
+while($row=mysqli_fetch_array($ShopDetais))
+{
+    $ShopIdForName = $row[2];
+    //echo $ShopIdForName ;
+    
+    
+    $SlotInShop = mysqli_query($con,"SELECT * FROM `shop_info` Where ShopID = '$ShopIdForName'");
+    while($row1=mysqli_fetch_array($SlotInShop))
+    {
+        $ShopSlotAvail = $row1[6];
+       // echo $ShopIdForName.'of'.$ShopSlotAvail;
+       //echo $ShopSlotAvail;
+       //check add already exist or not
+       if($ShopSlotAvail==0);
+           {
+              echo ' no slot available in this shop';
+           }
+      
+       if($ShopSlotAvail > 0)  {
+                   $getAdd = mysqli_query($con,"SELECT 	* FROM `market_shopads` where `AddID`='$add_id' and `ShopID`='$ShopIdForName'");
+                   $check = mysqli_fetch_array($getAdd);
+
+                    if($check == true)
+                     {
+                        echo "Add Already placed";
+                     }
+                   else 
+                     {
+                          echo $ShopIdForName.' '.$add_id.' '.$ShopSlotAvail.' '.$DueDate;
+                          $query ="INSERT INTO `market_shopads`(`ShopID`, `AddID`, `SoltNumber`, `DueDate`) VALUES ('$ShopIdForName','$add_id','$ShopSlotAvail','$DueDate')";
+                          $insertIntoShopSlot = mysqli_query($con,$query );
+                          if($insertIntoShopSlot == true)
+                            {
+                                echo "inserted";
+                                $updateShop = mysqli_query($con,"UPDATE `shop_info` SET `AddSlots` = `AddSlots`-1 Where ShopID='$ShopIdForName'" );
+                             }
+                     }
+
+            }
+    }
+
+}  
+
+
+
+
+
+$shops = mysqli_query($con,"SELECT count(`ID`) FROM `market_shopads` WHERE `AddID` = '$add_id'");
+while($row=mysqli_fetch_array($shops)){
+    $numberofshops = $row[0];
+    //$numberofproducts = $row[1];
+    echo $numberofshops;
+  //  echo $numberofproducts ;
+}
+
+
+
+
+//User Count
 $TotalUserCount = 0;
 $usersTrans = mysqli_query($con,"SELECT * FROM `user_transactions` WHERE `ProductID` = '$productID'");
 while($row=mysqli_fetch_array($usersTrans)){
@@ -55,11 +124,21 @@ while($row=mysqli_fetch_array($usersTrans)){
     }
 }  
 }
+
+
+
+
+
 //echo $counts;
 //echo $TotalUserCount;
-
-$query="INSERT INTO `market_ads`(`invID`, `ProductID`, `AddID`, `NoOfShops`, `NoOfUsers`) VALUES ('$id','$productID','$add_id','$numberofshops','$TotalUserCount')";
+$checkmarker_ads = mysqli_query($con,"SELECT * FROM `market_ads` Where AddID = '$add_id'");
+$result2 = mysqli_fetch_array($checkmarker_ads);
+if($result2 == true){
+    echo "Alreday exist";
+}
+else {
+$query="INSERT INTO `market_ads`(`invID`, `ProductID`, `AddID`, `NoOfShops`, `NoOfUsers`,`DueDate`) VALUES ('$id','$productID','$add_id','$numberofshops','$TotalUserCount','$DueDate')";
 $result = mysqli_query($con,$query);
-
+}
 }
 ?>
