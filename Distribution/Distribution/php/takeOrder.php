f22b764027aa63506cee516b34c9e52ac2236cc3
ANIL MN <amn.nmb@gmail.com>
2020-03-18 19:03:28 +0530

    Update takeOrder.php

--- Distribution/Distribution/php/takeOrder.php
+++ Distribution/Distribution/php/takeOrder.php
@@ -9,10 +9,12 @@ $disID=$_SESSION['phn_No'];
 
 $filename = basename($_SERVER['REQUEST_URI']);
 $ReqestID =substr($filename,14);
+
 echo $ReqestID;
 
 
 $query = "UPDATE `distributor_orders` SET `Status` = 'Order Purchased' where `Request_ID`='$ReqestID'";
+$query1 = mysqli_query($con,"UPDATE `dis_shopreq` SET `Status` = 'Order Purchased' where `ReqID`='$ReqestID'");
 $result = mysqli_query($con,$query);
 header('Location: ../home.php');
 
