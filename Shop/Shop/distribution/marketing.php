a15231d6d175c261c2826b1f826b66ed4df8186a
ANIL MN <amn.nmb@gmail.com>
2020-03-27 22:03:43 +0530

    Marktng Processing completed

--- Shop/Shop/distribution/marketing.php
+++ Shop/Shop/distribution/marketing.php
@@ -57,13 +57,13 @@ include('disreq.php');
       <div class="navbar-menu-wrapper d-flex align-items-center">
         <ul class="navbar-nav navbar-nav-left header-links d-none d-md-flex">
           <li class="nav-item">
-            <a href="#" class="nav-link"><i class="mdi mdi-image-filter"></i>User</a>
+            <a href="#slot" class="nav-link"><i class="mdi mdi-image-filter"></i>ADD Slot</a>
           </li>
           <li class="nav-item active">
-            <a href="#" class="nav-link"><i class="mdi mdi-email-outline"></i>Distribution</a>
+            <a href="#details" class="nav-link"><i class="mdi mdi-email-outline"></i>Slot Details</a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link"><i class="mdi mdi-calendar"></i>My Shop</a>
+            <a href="#ads" class="nav-link"><i class="mdi mdi-calendar"></i>MyADS</a>
           </li>
         </ul>
         <ul class="navbar-nav navbar-nav-right">
@@ -121,9 +121,9 @@ include('disreq.php');
                 <div class="badge badge-teal mx-auto mt-3">Online</div>
               </div>
             </div>   </li>
-          <li class="nav-item"><a class="nav-link" href="request.php"><img class="menu-icon" src="../images/menu_icons/01.png" alt="menu icon"><span class="menu-title">Request</span></a></li>
-          <li class="nav-item"><a class="nav-link" href="track.php"><img class="menu-icon" src="../images/menu_icons/02.png" alt="menu icon"><span class="menu-title">Track</span></a></li>
-          <li class="nav-item"><a class="nav-link" href="userinfo.php"><img class="menu-icon" src="../images/menu_icons/03.png" alt="menu icon"><span class="menu-title">User Information</span></a></li>
+          <li class="nav-item"><a class="nav-link" href=""><img class="menu-icon" src="../images/menu_icons/01.png" alt="menu icon"><span class="menu-title">Request</span></a></li>
+          <li class="nav-item"><a class="nav-link" href=""><img class="menu-icon" src="../images/menu_icons/02.png" alt="menu icon"><span class="menu-title">Track</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="../userinfo.php"><img class="menu-icon" src="../images/menu_icons/03.png" alt="menu icon"><span class="menu-title">User Information</span></a></li>
           <!-- <li class="nav-item"><a class="nav-link" href="../../../pages/forms/basic_elements.html"><img class="menu-icon" src="../../images/menu_icons/04.png" alt="menu icon"><span class="menu-title">Form</span></a></li>
           <li class="nav-item"><a class="nav-link" href="../../../pages/charts/chartjs.html"><img class="menu-icon" src="../../images/menu_icons/05.png" alt="menu icon"><span class="menu-title">Charts</span></a></li>
           <li class="nav-item"><a class="nav-link" href="../../../pages/tables/basic-table.html"><img class="menu-icon" src="../../images/menu_icons/06.png" alt="menu icon"><span class="menu-title">Table</span></a></li>
@@ -131,7 +131,7 @@ include('disreq.php');
           <li class="nav-item"> -->
             
           </li>
-            <li class="nav-item purchase-button"><a class="nav-link" href="" target="_blank">BACK</a></li>
+            <li class="nav-item purchase-button"><a class="nav-link" href="../UserInfo.php" >BACK</a></li>
         </ul>
       </nav>
       <!-- partial -->
@@ -139,7 +139,7 @@ include('disreq.php');
          <div class="content-wrapper">
            <div class="row" >
                <div class="col-lg-6 grid-margin stretch-card">
-                  <div class="card">
+                  <div class="card" id="slot">
                       <div class="card-body">
                          <form action="#" method='POST'> 
                            <h4 class="card-title">Add Slots </h4>
@@ -175,7 +175,7 @@ include('disreq.php');
 
             <div class="col-lg-6 grid-margin stretch-card">
               <div class="card">
-                <div class="card-body">
+                <div class="card-body" id='details'>
                   <form action="#" method='POST'> 
                      <h4 class="card-title">Add Slot Details</h4>
                       <p class="card-description"> </p>
@@ -209,8 +209,18 @@ include('disreq.php');
                       {
                         $quantity=$_POST['quantity'];
                         $slotnum = $_POST['SlotNum'];
-                        $query="INSERT INTO `market_shopslots`(`ShopID`,`SlotNumber`,`SlotName`) VALUES ('$shopid','$slotnum','$quantity')";
-                        $res=mysqli_query($con,$query);
+                        include('../../../BackEnd/php/connection.php');
+                        $AddslotDetails = mysqli_query($con,"SELECT * FROM `market_shopslots` WHERE `SlotNumber`= '$slotnum' and `SlotName`='$quantity'");
+                        $row=mysqli_fetch_array($AddslotDetails);
+                        if($row==true){
+
+                        }
+                        else{
+                          $query="INSERT INTO `market_shopslots`(`ShopID`,`SlotNumber`,`SlotName`) VALUES ('$shopid','$slotnum','$quantity')";
+                          $res=mysqli_query($con,$query);
+    
+                        } 
+                      
                         
                        }
                       
@@ -222,7 +232,47 @@ include('disreq.php');
                   </div>
                 </div>    
               </div>
+              <div class="row" id='ads'>
+              <?php
+               include('../../../BackEnd/php/connection.php');
+               $AddslotDetails = mysqli_query($con,"SELECT * FROM `market_shopslots` WHERE `ShopID`= '$shopid'");
+               while($row=mysqli_fetch_array($AddslotDetails))
+               {
+                 $slotID=$row[2];
+                 $slotName=$row[3];
+                 $addInfo = mysqli_query($con,"SELECT * FROM `market_shopads` where `SoltNumber`='$slotID'");
+                 while($row1=mysqli_fetch_array($addInfo))
+                 {
+                   $addid = $row1[2];
+                 }
+                   $addname1 = mysqli_query($con,"SELECT * FROM `market_add` where `Add_ID`='$addid'");
+                   while($row2=mysqli_fetch_array($addname1))
+                   {
 
+                      $addname = $row2[2];
+                      echo '<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="clearfix">
+                            <i class="fa fa-users float-right icon-md text-gray"></i>
+                          </div>
+                          <img src="../../../User/user/img/ProductImages/10003.jpg" style="height:150px; width:150px; margin-left:20px;  margin-bottom:40px;">
+                          <div >
+                          <h4 class="card-title font-weight-normal text-info">'.$addname.'</h4>
+                          <h6 class="card-subtitle mb-4">'.$slotName.'</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>';
+                   }
+                
+               }
+    
+
+
+             ?>
+             
+          </div>
 
                   
         <!-- content-wrapper ends -->
@@ -230,7 +280,7 @@ include('disreq.php');
         <footer class="footer">
           <div class="container-fluid clearfix">
             <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">RTEA© ANIL <a href="http://www.bootstrapdash.com/" target="_blank"></span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> <i class="mdi mdi-heart text-danger"></i></span>
           </div>
         </footer>
         <!-- partial -->
