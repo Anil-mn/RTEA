5912f10ef86fac57f469e4d80c93a534cba0ddb2
ANIL MN <amn.nmb@gmail.com>
2020-04-01 20:37:11 +0530

    updated trial.py

--- Data Analysis/trial.py
+++ Data Analysis/trial.py
@@ -3,12 +3,36 @@ from connection import mydb
 import matplotlib.pyplot as plt
 mycursor = mydb.cursor()
 import seaborn as sns
+import pandas as pd
+import numpy as np
+
+
 mycursor.execute("SELECT `Gender` FROM `user_info`")
 
 myresult = mycursor.fetchall()
+#print(type(myresult))
+
+
+# myarray = np.asarray(myresult)
+# print(type(myresult))
+# print (myresult)
+
+
+arry=[]
 # print(type(myresult))
 for x in myresult:
-  print(x)
+    for i in x:
+        #print(type(i))
+        arry.append(i) 
+    #arry =arry.insert(x)
+
+#print(arry)
+#df = pd.DataFrame({'Gender':arry})
+df = pd.DataFrame([arry])
+df.head()
+df.columns =['Gender','Gender','Gender','Gender']
+print(df.head())
+
 
 # plt.figure(figsize=(10,6))
 # plt.title("Ages Frequency")
@@ -17,8 +41,8 @@ for x in myresult:
 # plt.show() 
 
 #print(myresult[0])
-# genders = myresult[5].Gender.value_counts()
-# sns.set_style("darkgrid")
-# plt.figure(figsize=(10,10))
-# sns.barplot(x=genders.index, y=genders.values)
-# plt.show()
+genders = df.Gender.value_counts()
+sns.set_style("darkgrid")
+plt.figure(figsize=(10,10))
+sns.barplot(x=genders.index, y=genders.values)
+plt.show()
